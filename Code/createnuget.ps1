15f65c43b6a3005eb4fd936a4f71067ab26af2dc
Petter Ekrann <petter@ekrann.no>
2019-02-15 11:56:16 +0100

    Delete createnuget.ps1

--- Code/createnuget.ps1
+++
@@ -1,8 +0,0 @@
-Import-Module ItasBuildNuget -Verbose -Force
-
-$Feed = "Default"
-$IncludeReferencedProjects = $false;
-$Output = "nuget";
-
-ItasBuildNuget -Root $PSScriptRoot -Output $Output -Configuration "Debug" -Nuspec "" -Feed $Feed
-Read-Host 'Script finished. Press Enter to exit' | Out-Null
